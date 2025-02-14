3337b3b2e2774d1634494ae0d9a8fe634dae5416
Carlos Aucancela <carlos.aucancela@infotrack.com>
2025-02-14 00:21:59 -0500

    CORS allowed

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-nel+&+!htun97nqdhu@$+i4i)q(sh#8!u7@kf(sz%@yyr1innp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-d15e.up.railway.app','localhost','*']
+ALLOWED_HOSTS = ['web-production-d15e.up.railway.app','localhost','*','localhost:3000']
 
 
 # Application definition
